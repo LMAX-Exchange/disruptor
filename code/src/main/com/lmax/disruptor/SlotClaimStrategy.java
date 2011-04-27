42d6c2920ff2385582fc7ae3df0fd0f940125cca
Martin Thompson <mjpt777@gmail.com>
2011-04-27 08:39:05 +0000

    Added javadoc

--- code/src/main/com/lmax/disruptor/SlotClaimStrategy.java
+++ code/src/main/com/lmax/disruptor/SlotClaimStrategy.java
@@ -1,7 +1,24 @@
 package com.lmax.disruptor;
 
-public interface SlotClaimStrategy
+/**
+ * Implementations of this strategy can handled the necessary threading requirements
+ * for claiming slots in the {@link RingBuffer}.
+ *
+ * The slot index is a the sequence value mod the {@link RingBuffer} capacity.
+ */
+interface SlotClaimStrategy
 {
+    /**
+     * Claim the next sequence index in the {@link RingBuffer} and increment.
+     *
+     * @return the slot index to be used for the producer.
+     */
     long getAndIncrement();
+
+    /**
+     * Set the current sequence value for claiming slots in the {@link RingBuffer}
+     *
+     * @param sequence to be set as the current value.
+     */
     void setSequence(long sequence);
 }
