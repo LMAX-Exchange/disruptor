37920d6d711c3a6b075b8c3eddb2de3cc0418080
Martin Thompson <mjpt777@gmail.com>
2011-05-01 20:44:50 +0000

    Javadoc

--- code/src/main/com/lmax/disruptor/EntryConsumer.java
+++ code/src/main/com/lmax/disruptor/EntryConsumer.java
@@ -21,7 +21,8 @@ public interface EntryConsumer<T extends  Entry> extends Runnable
 
     /**
      * Signal that this EntryConsumer should stop when it has finished consuming at the next clean break.
-     * If thread is waiting on a {@link ThresholdBarrier} is it necessary to alert the thread to check status.
+     * If thread is waiting on a {@link ThresholdBarrier} is it necessary to call {@link ThresholdBarrier#alert()}
+     * to notify the thread to check status.
      */
     void halt();
 }
