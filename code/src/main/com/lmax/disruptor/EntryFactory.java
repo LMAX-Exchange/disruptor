4fb0e4d479c7413d3c808f4a2583d00f57d4313e
Martin Thompson <mjpt777@gmail.com>
2011-05-03 16:49:08 +0000

    Improved Javadoc

--- code/src/main/com/lmax/disruptor/EntryFactory.java
+++ code/src/main/com/lmax/disruptor/EntryFactory.java
@@ -1,7 +1,7 @@
 package com.lmax.disruptor;
 
 /**
- * Called by the {@link RingBuffer} to pre-populate all the {@link Entry} in the ring.
+ * Called by the {@link RingBuffer} to pre-populate all the {@link Entry}s to fill the RingBuffer.
  * 
  * @param <T> Entry implementation storing the data for sharing during exchange or parallel coordination of an event.
  */
