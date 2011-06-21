68e21997b4f42da2b15e16f59597f4aae4805789
Martin Thompson <mjpt777@gmail.com>
2011-06-21 09:00:56 +0000

    Only calculate total for interval when count is greater than zero.

--- code/src/main/com/lmax/disruptor/collections/Histogram.java
+++ code/src/main/com/lmax/disruptor/collections/Histogram.java
@@ -220,17 +220,26 @@ public final class Histogram
      */
     public BigDecimal getMean()
     {
+        if (0L == getCount())
+        {
+            return BigDecimal.ZERO;
+        }
+
         long lowerBound = counts[0] > 0L ? minValue : 0L;
         BigDecimal total = BigDecimal.ZERO;
 
         for (int i = 0, size = upperBounds.length; i < size; i++)
         {
-            long upperBound = Math.min(upperBounds[i], maxValue);
-            long midPoint = lowerBound + ((upperBound - lowerBound) / 2L);
+            if (0L != counts[i])
+            {
+                long upperBound = Math.min(upperBounds[i], maxValue);
+                long midPoint = lowerBound + ((upperBound - lowerBound) / 2L);
 
-            BigDecimal intervalTotal = new BigDecimal(midPoint).multiply(new BigDecimal(counts[i]));
-            total = total.add(intervalTotal);
-            lowerBound = Math.max(upperBounds[i] + 1L, minValue);
+                BigDecimal intervalTotal = new BigDecimal(midPoint).multiply(new BigDecimal(counts[i]));
+                total = total.add(intervalTotal);
+
+                lowerBound = Math.max(upperBounds[i] + 1L, minValue);
+            }
         }
 
         return total.divide(new BigDecimal(getCount()), 2, RoundingMode.HALF_UP);
