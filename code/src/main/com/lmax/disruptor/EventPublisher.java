c4f607b050e5987a97e22750ad71c7184aa8b65c
Michael Barker <mikeb01@gmail.com>
2011-09-03 10:46:20 +0000

    Added javadoc for EventPublisher

--- code/src/main/com/lmax/disruptor/EventPublisher.java
+++ code/src/main/com/lmax/disruptor/EventPublisher.java
@@ -1,14 +1,29 @@
 package com.lmax.disruptor;
 
+/**
+ * Utility class for simplifying publication to the ring buffer.
+ */
 public class EventPublisher<E>
 {
     private final RingBuffer<E> ringBuffer;
 
+    /**
+     * Construct from the ring buffer to be published to.
+     * @param ringBuffer
+     */
     public EventPublisher(RingBuffer<E> ringBuffer)
     {
         this.ringBuffer = ringBuffer;
     }
     
+    /**
+     * Publishes an event to the ring buffer.  It handles
+     * claiming the next sequence, getting the current (uninitialized) 
+     * event from the ring buffer and publishing the claimed sequence
+     * after translation.
+     * 
+     * @param translator The user specified translation for the event
+     */
     public void publishEvent(EventTranslator<E> translator)
     {
         long sequence = ringBuffer.nextSequence();
