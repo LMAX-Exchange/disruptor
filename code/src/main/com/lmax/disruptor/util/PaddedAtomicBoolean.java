5538a708bac5cf862ef5c35c921990d41a616766
Martin Thompson <mjpt777@gmail.com>
2011-09-05 08:20:26 +0000

    DependencyBarrier to SequenceBarrier name change and added PaddedAtomicBoolean

--- code/src/main/com/lmax/disruptor/util/PaddedAtomicBoolean.java
+++ code/src/main/com/lmax/disruptor/util/PaddedAtomicBoolean.java
@@ -7,7 +7,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
  */
 public class PaddedAtomicBoolean extends AtomicBoolean
 {
-    public volatile long p1, p2, p3, p4, p5, p6 = 7L;
+    public volatile long p1, p2, p3, p4, p5, p6, p7 = 7L;
 
     /**
      * Default constructor
