9e45677f8779c859bff6fed3cba3ea666bde0b03
Martin Thompson <mjpt777@gmail.com>
2011-11-03 20:07:50 +0000

    Removed unused imports.

--- code/src/test/com/lmax/disruptor/MultiThreadedClaimStrategyTest.java
+++ code/src/test/com/lmax/disruptor/MultiThreadedClaimStrategyTest.java
@@ -24,8 +24,7 @@ import org.junit.runner.RunWith;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
+
 import java.util.concurrent.atomic.AtomicReferenceArray;
 
 import static junit.framework.Assert.*;
