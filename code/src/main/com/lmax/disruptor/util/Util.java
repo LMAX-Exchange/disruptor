a2531b39187d7c8ffa8f7e390b0dd1537b4938d6
Michael Barker <mikeb01@gmail.com>
2012-08-27 14:26:42 +0000

    Add privileged action around getting the Unsafe

--- code/src/main/com/lmax/disruptor/util/Util.java
+++ code/src/main/com/lmax/disruptor/util/Util.java
@@ -16,6 +16,10 @@
 package com.lmax.disruptor.util;
 
 import java.lang.reflect.Field;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.security.AccessController;
+import java.security.PrivilegedExceptionAction;
 
 import sun.misc.Unsafe;
 
@@ -80,9 +84,17 @@ public final class Util
     {
         try
         {
-            Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
-            theUnsafe.setAccessible(true);
-            THE_UNSAFE = (Unsafe)theUnsafe.get(null);
+            final PrivilegedExceptionAction<Unsafe> action = new PrivilegedExceptionAction<Unsafe>()
+            {
+                public Unsafe run() throws Exception
+                {
+                    Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+                    theUnsafe.setAccessible(true);
+                    return (Unsafe) theUnsafe.get(null);                    
+                }
+            };
+            
+            THE_UNSAFE = AccessController.doPrivileged(action);
         }
         catch (Exception e)
         {
@@ -90,8 +102,50 @@ public final class Util
         }        
     }
     
+    /**
+     * Get a handle on the Unsafe instance, used for accessing low-level concurrency
+     * and memory constructs.
+     * @return The Unsafe
+     */
     public static Unsafe getUnsafe()
     {
         return THE_UNSAFE;
+    }
+    
+    /**
+     * Gets the address value for the memory that backs a direct byte buffer.
+     * @param buffer
+     * @return The system address for the buffers
+     */
+    public static long getAddressFromDirectByteBuffer(ByteBuffer buffer)
+    {
+        try
+        {            
+            Field addressField = Buffer.class.getDeclaredField("address");
+            addressField.setAccessible(true);
+            return addressField.getLong(buffer);
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException("Unable to address field from ByteBuffer", e);
+        }
+    }
+    
+    
+    /**
+     * Calculate the log base 2 of the supplied integer, essentially reports the location
+     * of the highest bit.
+     * 
+     * @param i Value to calculate log2 for.
+     * @return The log2 value
+     */
+    public static int log2(int i)
+    {
+        int r = 0;
+        while ((i >>= 1) != 0)
+        {
+            ++r;
+        }
+        return r;
     }
 }
