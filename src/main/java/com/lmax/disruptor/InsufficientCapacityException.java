7d51906acd9e613996739ce2938fa786563e0f3a
Michael Barker <mikeb01@gmail.com>
2012-11-30 15:54:13 +0000

    Add javadoc for InsufficientCapacityException

--- src/main/java/com/lmax/disruptor/InsufficientCapacityException.java
+++ src/main/java/com/lmax/disruptor/InsufficientCapacityException.java
@@ -15,6 +15,15 @@
  */
 package com.lmax.disruptor;
 
+/**
+ * <p>Exception thrown when the it is not possible to insert a value into
+ * the ring buffer without it wrapping the consuming sequenes.  Used
+ * specifically when claiming with the {@link RingBuffer#tryNext()} call.
+ * 
+ * <p>For efficiency this exception will not have a stack trace.
+ * @author mikeb01
+ *
+ */
 @SuppressWarnings("serial")
 public class InsufficientCapacityException extends Exception
 {
