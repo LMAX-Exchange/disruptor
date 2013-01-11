11560288a974cd49d2e86e5d2c51fec20a66d195
jasonk000 <jasonk@bluedevel.com>
2013-01-11 17:57:12 +1100

    Added detailed comments for MultiProducerPublisher
    Explains the availability flag check logic

--- src/main/java/com/lmax/disruptor/MultiProducerPublisher.java
+++ src/main/java/com/lmax/disruptor/MultiProducerPublisher.java
@@ -31,7 +31,8 @@ class MultiProducerPublisher implements Publisher
     private static final long scale = UNSAFE.arrayIndexScale(int[].class);
     
     private final WaitStrategy waitStrategy;
-    // int[] tracks the state of each ringbuffer slot
+    // availableBuffer tracks the state of each ringbuffer slot
+    // see below for more details on the approach
     private final int[] availableBuffer;
     private final int indexMask;
     private final int indexShift;
@@ -63,6 +64,25 @@ class MultiProducerPublisher implements Publisher
         waitStrategy.signalAllWhenBlocking();
     }
 
+    /** 
+     * The below methods work on the availableBuffer flag.
+     * 
+     * The prime reason is to avoid a shared sequence object between publisher threads.
+     * (Keeping single pointers tracking start and end would require coordination 
+     * between the threads). 
+     * 
+     * --  Firstly we have the constraint that the delta between the cursor and minimum
+     * gating sequence will never be larger than the buffer size (the code in 
+     * next/tryNext in the Sequence takes care of that).
+     * -- Given that; take the sequence value and mask off the lower portion of the
+     * sequence as the index into the buffer (indexMask). (aka modulo operator)
+     * -- The upper portion of the sequence becomes the value to check for availability.
+     * ie: it tells us how many times around the ring buffer we've been (aka division)
+     * -- Beause we can't wrap without the gating sequences moving forward (i.e. the
+     * minimum gating sequence is effectively our last available position in the
+     * buffer), when we have new data and successfully claimed a slot we can simply
+     * write over the top.
+     */
     private void setAvailable(final long sequence)
     {
         setAvailableBufferValue(calculateIndex(sequence), calculateAvailabilityFlag(sequence));
