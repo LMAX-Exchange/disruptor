98238e080cca56cb4756524b3cdfca20184b2768
Michael Barker <mikeb01@gmail.com>
2013-04-09 16:01:14 +1200

    [Mike] Add raw sequencer performance test back

---
+++ src/perftest/java/com/lmax/disruptor/OnePublisherToOneProcessorRawThroughputTest.java
@@ -0,0 +1,188 @@
+/*
+ * Copyright 2011 LMAX Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.lmax.disruptor;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.junit.Test;
+
+import com.lmax.disruptor.util.DaemonThreadFactory;
+
+/**
+ * <pre>
+ * UniCast a series of items between 1 publisher and 1 event processor.
+ *
+ * +----+    +-----+
+ * | P1 |--->| EP1 |
+ * +----+    +-----+
+ *
+ *
+ * Queue Based:
+ * ============
+ *
+ *        put      take
+ * +----+    +====+    +-----+
+ * | P1 |--->| Q1 |<---| EP1 |
+ * +----+    +====+    +-----+
+ *
+ * P1  - Publisher 1
+ * Q1  - Queue 1
+ * EP1 - EventProcessor 1
+ *
+ *
+ * Disruptor:
+ * ==========
+ *              track to prevent wrap
+ *              +------------------+
+ *              |                  |
+ *              |                  v
+ * +----+    +====+    +====+   +-----+
+ * | P1 |--->| RB |<---| SB |   | EP1 |
+ * +----+    +====+    +====+   +-----+
+ *      claim      get    ^        |
+ *                        |        |
+ *                        +--------+
+ *                          waitFor
+ *
+ * P1  - Publisher 1
+ * RB  - RingBuffer
+ * SB  - SequenceBarrier
+ * EP1 - EventProcessor 1
+ *
+ * </pre>
+ */
+public final class OnePublisherToOneProcessorRawThroughputTest extends AbstractPerfTestQueueVsDisruptor
+{
+    private static final int BUFFER_SIZE = 1024 * 64;
+    private static final long ITERATIONS = 1000L * 1000L * 200L;
+    private final ExecutorService EXECUTOR = Executors.newSingleThreadExecutor(DaemonThreadFactory.INSTANCE);
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    private final Sequencer sequencer = new SingleProducerSequencer(BUFFER_SIZE, new YieldingWaitStrategy());
+    private final MyRunnable myRunnable = new MyRunnable(sequencer);
+    {
+        sequencer.addGatingSequences(myRunnable.sequence);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    protected int getRequiredProcessorCount()
+    {
+        return 2;
+    }
+
+    @Test
+    @Override
+    public void shouldCompareDisruptorVsQueues() throws Exception
+    {
+        testImplementations();
+    }
+
+    @Override
+    protected long runQueuePass() throws InterruptedException
+    {
+        return 0;
+    }
+
+    @Override
+    protected long runDisruptorPass() throws InterruptedException
+    {
+        final CountDownLatch latch = new CountDownLatch(1);
+        long expectedCount = myRunnable.sequence.get() + ITERATIONS;
+        myRunnable.reset(latch, expectedCount);
+        EXECUTOR.submit(myRunnable);
+        long start = System.currentTimeMillis();
+        
+        final Sequencer sequencer = this.sequencer;
+
+        for (long i = 0; i < ITERATIONS; i++)
+        {
+            long next = sequencer.next();
+            sequencer.publish(next);
+        }
+
+        latch.await();
+        long opsPerSecond = (ITERATIONS * 1000L) / (System.currentTimeMillis() - start);
+        waitForEventProcessorSequence(expectedCount);
+
+        return opsPerSecond;
+    }
+
+    private void waitForEventProcessorSequence(long expectedCount) throws InterruptedException
+    {
+        while (myRunnable.sequence.get() != expectedCount)
+        {
+            Thread.sleep(1);
+        }
+    }
+    
+    private static class MyRunnable implements Runnable
+    {
+        private CountDownLatch latch;
+        private long expectedCount;
+        Sequence sequence = new Sequence(-1);
+        private SequenceBarrier barrier;
+
+        public MyRunnable(Sequencer sequencer)
+        {
+            this.barrier = sequencer.newBarrier();
+        }
+        
+        public void reset(CountDownLatch latch, long expectedCount)
+        {
+            this.latch = latch;
+            this.expectedCount = expectedCount;
+        }
+
+        @Override
+        public void run()
+        {
+            long expected = expectedCount;
+            long processed = -1;
+            
+            try
+            {
+                do
+                {
+                    processed = barrier.waitFor(sequence.get() + 1);
+                    sequence.set(processed);
+                }
+                while (processed < expected);
+                
+                latch.countDown();
+                sequence.setVolatile(processed);
+            }
+            catch (AlertException e)
+            {
+                e.printStackTrace();
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public static void main(String[] args) throws Exception
+    {
+        OnePublisherToOneProcessorRawThroughputTest test = new OnePublisherToOneProcessorRawThroughputTest();
+        test.shouldCompareDisruptorVsQueues();
+    }
+}
