9bf142c5d4cc7aee874b3352a034ed08e59f4f0a
Michael Barker (barkerm) <mikeb01@gmail.com>
2013-08-09 10:11:16 +1200

    Remove unnecessary 2nd await

--- src/perftest/java/com/lmax/disruptor/PingPongLatencyTest.java
+++ src/perftest/java/com/lmax/disruptor/PingPongLatencyTest.java
@@ -175,7 +175,6 @@ public final class PingPongLatencyTest
         barrier.await();
         latch.await();
 
-        latch.await();
         pingProcessor.halt();
         pongProcessor.halt();
     }
