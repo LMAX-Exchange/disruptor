0cebc08164947a4054ea6613c718f9d03d416040
Michael Barker <mikeb01@gmail.com>
2013-12-02 11:09:23 +1300

    Split queue test out from OneToOneSequenedThroughputTest

--- src/perftest/java/com/lmax/disruptor/sequenced/OneToOneSequencedThroughputTest.java
+++ src/perftest/java/com/lmax/disruptor/sequenced/OneToOneSequencedThroughputTest.java
@@ -17,18 +17,21 @@ package com.lmax.disruptor.sequenced;
 
 import static com.lmax.disruptor.RingBuffer.createSingleProducer;
 
-import com.lmax.disruptor.AbstractPerfTestQueueVsDisruptor;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.junit.Assert;
+
+import com.lmax.disruptor.AbstractPerfTestDisruptor;
 import com.lmax.disruptor.BatchEventProcessor;
 import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.SequenceBarrier;
 import com.lmax.disruptor.YieldingWaitStrategy;
-import com.lmax.disruptor.support.*;
+import com.lmax.disruptor.support.PerfTestUtil;
+import com.lmax.disruptor.support.ValueAdditionEventHandler;
+import com.lmax.disruptor.support.ValueEvent;
 import com.lmax.disruptor.util.DaemonThreadFactory;
-
-import org.junit.Assert;
-import org.junit.Test;
-
-import java.util.concurrent.*;
 
 /**
  * <pre>
@@ -37,20 +40,6 @@ import java.util.concurrent.*;
  * +----+    +-----+
  * | P1 |--->| EP1 |
  * +----+    +-----+
- *
- *
- * Queue Based:
- * ============
- *
- *        put      take
- * +----+    +====+    +-----+
- * | P1 |--->| Q1 |<---| EP1 |
- * +----+    +====+    +-----+
- *
- * P1  - Publisher 1
- * Q1  - Queue 1
- * EP1 - EventProcessor 1
- *
  *
  * Disruptor:
  * ==========
@@ -73,17 +62,12 @@ import java.util.concurrent.*;
  *
  * </pre>
  */
-public final class OneToOneSequencedThroughputTest extends AbstractPerfTestQueueVsDisruptor
+public final class OneToOneSequencedThroughputTest extends AbstractPerfTestDisruptor
 {
     private static final int BUFFER_SIZE = 1024 * 64;
     private static final long ITERATIONS = 1000L * 1000L * 100L;
     private final ExecutorService executor = Executors.newSingleThreadExecutor(DaemonThreadFactory.INSTANCE);
     private final long expectedResult = PerfTestUtil.accumulatedAddition(ITERATIONS);
-
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-
-    private final BlockingQueue<Long> blockingQueue = new LinkedBlockingQueue<Long>(BUFFER_SIZE);
-    private final ValueAdditionQueueProcessor queueProcessor = new ValueAdditionQueueProcessor(blockingQueue, ITERATIONS - 1);
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -102,36 +86,6 @@ public final class OneToOneSequencedThroughputTest extends AbstractPerfTestQueue
     protected int getRequiredProcessorCount()
     {
         return 2;
-    }
-
-    @Test
-    @Override
-    public void shouldCompareDisruptorVsQueues() throws Exception
-    {
-        testImplementations();
-    }
-
-    @Override
-    protected long runQueuePass() throws InterruptedException
-    {
-        final CountDownLatch latch = new CountDownLatch(1);
-        queueProcessor.reset(latch);
-        Future<?> future = executor.submit(queueProcessor);
-        long start = System.currentTimeMillis();
-
-        for (long i = 0; i < ITERATIONS; i++)
-        {
-            blockingQueue.put(Long.valueOf(i));
-        }
-
-        latch.await();
-        long opsPerSecond = (ITERATIONS * 1000L) / (System.currentTimeMillis() - start);
-        queueProcessor.halt();
-        future.cancel(true);
-
-        Assert.assertEquals(expectedResult, queueProcessor.getValue());
-
-        return opsPerSecond;
     }
 
     @Override
@@ -173,6 +127,6 @@ public final class OneToOneSequencedThroughputTest extends AbstractPerfTestQueue
     public static void main(String[] args) throws Exception
     {
         OneToOneSequencedThroughputTest test = new OneToOneSequencedThroughputTest();
-        test.shouldCompareDisruptorVsQueues();
+        test.testImplementations();
     }
 }
