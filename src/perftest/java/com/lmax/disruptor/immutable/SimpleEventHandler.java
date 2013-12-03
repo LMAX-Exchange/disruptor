ba0e2b2fdf71a8ebcfe5453cd84ce42618ef4c3c
Michael Barker <mikeb01@gmail.com>
2013-12-04 09:50:10 +1300

    Add JNA, actually do work in SimpleEventHandler

--- src/perftest/java/com/lmax/disruptor/immutable/SimpleEventHandler.java
+++ src/perftest/java/com/lmax/disruptor/immutable/SimpleEventHandler.java
@@ -4,8 +4,11 @@ import com.lmax.disruptor.EventHandler;
 
 public class SimpleEventHandler implements EventHandler<SimpleEvent>
 {
+    public long counter;
+
     @Override
     public void onEvent(SimpleEvent arg0, long arg1, boolean arg2) throws Exception
     {
+        counter += arg0.getCounter();
     }
 }
