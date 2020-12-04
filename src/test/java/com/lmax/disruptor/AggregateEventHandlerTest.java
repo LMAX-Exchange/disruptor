14daff3db8284619c30d9378a5ad9e3b8756426f
silantyeve <silantyeve@lmax.com>
2020-12-04 16:36:32 +0000

    Rewrite AggregateEventHandlerTest in spirit of JUnit 5

--- src/test/java/com/lmax/disruptor/AggregateEventHandlerTest.java
+++ src/test/java/com/lmax/disruptor/AggregateEventHandlerTest.java
@@ -16,10 +16,10 @@
 package com.lmax.disruptor;
 
 import com.lmax.disruptor.support.DummyEventHandler;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 @SuppressWarnings("unchecked")
 public final class AggregateEventHandlerTest
