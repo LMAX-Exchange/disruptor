b8c10314aaaf84f87b99c017e42d588d10b4e267
Nick Palmer <nick@palmr.co.uk>
2021-04-04 23:36:23 +0100

    Add in `module-info.java` for JDK module definition (still requires unsafe)
    Closes https://github.com/LMAX-Exchange/disruptor/issues/234

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module disruptor.main {
+    requires jdk.unsupported; // Temporarily required for sum.misc.Unsafe until Unsafe is removed
+
+    exports com.lmax.disruptor;
+    exports com.lmax.disruptor.dsl;
+    exports com.lmax.disruptor.util;
+}
