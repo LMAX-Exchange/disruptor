8687f029c5a837c617f8e71639aee2966791fb11
A248 <anandebeh@gmail.com>
2021-04-05 15:29:20 -0400

    Use platform logging API in Fatal/IgnoreExceptionHandler
    * Avoids initializing java.util.logging in the case where the user
      replaces the System.LoggerFinder implementation
    * Remove constructors taking a java.util.logging.Logger from both
      classes

--- src/main/java/com/lmax/disruptor/FatalExceptionHandler.java
+++ src/main/java/com/lmax/disruptor/FatalExceptionHandler.java
@@ -15,32 +15,22 @@
  */
 package com.lmax.disruptor;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.lang.System.Logger.Level;
+import java.lang.System.Logger;
 
 /**
- * Convenience implementation of an exception handler that using standard JDK logging to log
- * the exception as {@link Level}.SEVERE and re-throw it wrapped in a {@link RuntimeException}
+ * Convenience implementation of an exception handler that uses the standard JDK logging
+ * of {@link System.Logger} to log the exception as {@link Level}.ERROR and re-throw
+ * it wrapped in a {@link RuntimeException}
  */
 public final class FatalExceptionHandler implements ExceptionHandler<Object>
 {
-    private static final Logger LOGGER = Logger.getLogger(FatalExceptionHandler.class.getName());
-    private final Logger logger;
-
-    public FatalExceptionHandler()
-    {
-        this.logger = LOGGER;
-    }
-
-    public FatalExceptionHandler(final Logger logger)
-    {
-        this.logger = logger;
-    }
+    private static final Logger LOGGER = System.getLogger(FatalExceptionHandler.class.getName());
 
     @Override
     public void handleEventException(final Throwable ex, final long sequence, final Object event)
     {
-        logger.log(Level.SEVERE, "Exception processing: " + sequence + " " + event, ex);
+        LOGGER.log(Level.ERROR, "Exception processing: " + sequence + " " + event, ex);
 
         throw new RuntimeException(ex);
     }
@@ -48,12 +38,12 @@ public final class FatalExceptionHandler implements ExceptionHandler<Object>
     @Override
     public void handleOnStartException(final Throwable ex)
     {
-        logger.log(Level.SEVERE, "Exception during onStart()", ex);
+        LOGGER.log(Level.ERROR, "Exception during onStart()", ex);
     }
 
     @Override
     public void handleOnShutdownException(final Throwable ex)
     {
-        logger.log(Level.SEVERE, "Exception during onShutdown()", ex);
+        LOGGER.log(Level.ERROR, "Exception during onShutdown()", ex);
     }
 }
