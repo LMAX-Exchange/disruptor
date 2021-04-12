45d0e87eeba0512ffa5c149c136bc6d629628900
A248 <anandebeh@gmail.com>
2021-04-12 15:14:09 -0400

    Fix module-info module name

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module disruptor.main {
+module com.lmax.disruptor {
     requires jdk.unsupported; // Temporarily required for sum.misc.Unsafe until Unsafe is removed
 
     exports com.lmax.disruptor;
