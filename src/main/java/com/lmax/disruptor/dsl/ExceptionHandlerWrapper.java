fb8b36dc61cb774e6387f4f0bbc9481b35b83e31
horizonzy <1060026287@qq.com>
2021-06-11 14:11:35 +0800

    catch exception when delegate exception handler process.

--- src/main/java/com/lmax/disruptor/dsl/ExceptionHandlerWrapper.java
+++ src/main/java/com/lmax/disruptor/dsl/ExceptionHandlerWrapper.java
@@ -15,19 +15,40 @@ public class ExceptionHandlerWrapper<T> implements ExceptionHandler<T>
     @Override
     public void handleEventException(final Throwable ex, final long sequence, final T event)
     {
-        getExceptionHandler().handleEventException(ex, sequence, event);
+        try
+        {
+            getExceptionHandler().handleEventException(ex, sequence, event);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void handleOnStartException(final Throwable ex)
     {
-        getExceptionHandler().handleOnStartException(ex);
+        try
+        {
+            getExceptionHandler().handleOnStartException(ex);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void handleOnShutdownException(final Throwable ex)
     {
-        getExceptionHandler() .handleOnShutdownException(ex);
+        try
+        {
+            getExceptionHandler() .handleOnShutdownException(ex);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
     private ExceptionHandler<? super T> getExceptionHandler()
