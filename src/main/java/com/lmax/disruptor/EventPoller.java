eaa84129c409d9cb73dc45803c52324382d2a744
swarren12 <33803336+swarren12@users.noreply.github.com>
2021-10-06 14:44:55 +0100

    Use a logical rather than bitwise AND in `EventPoller`

--- src/main/java/com/lmax/disruptor/EventPoller.java
+++ src/main/java/com/lmax/disruptor/EventPoller.java
@@ -109,7 +109,7 @@ public class EventPoller<T>
                     nextSequence++;
 
                 }
-                while (nextSequence <= availableSequence & processNextEvent);
+                while (nextSequence <= availableSequence && processNextEvent);
             }
             finally
             {
