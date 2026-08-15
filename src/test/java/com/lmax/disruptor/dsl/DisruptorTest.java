7fc5ffd8b433b7fab6b16836a3c992b1efd884d1
arimu1 <19286898+arimu1@users.noreply.github.com>
2026-08-15 10:04:03 +0700

    test(dsl): drop unused assertNull import

--- src/test/java/com/lmax/disruptor/dsl/DisruptorTest.java
+++ src/test/java/com/lmax/disruptor/dsl/DisruptorTest.java
@@ -56,7 +56,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
